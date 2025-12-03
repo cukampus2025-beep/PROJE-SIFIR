8b9c8717765f881d60f4e64160fde61ab866af0c
cukampus2025-beep <cukampus2025@gmail.com>
2025-12-03 18:01:05 +0300

    guncellemer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -281,7 +281,7 @@ function AnaSayfa() {
   return (
     <div className="main-container">
       <GlobalStyles /> 
-      <div className="beta-text">Beta 0.3</div>
+      <div className="beta-text">Beta 0.32</div>
       <GirisModal kapali={!modalAcik} kapat={() => setModalAcik(false)} tip={modalTip} />
       
       <div className="hamburger-fixed" style={{
